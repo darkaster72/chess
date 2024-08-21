d24d01224817b4d48804f05c12e21536ebfc1270
Ankit Sharma <sankit72@gmail.com>
2024-08-21 23:56:23 +0530

    add chess board

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module io.github.darkaster.chess {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+    requires org.kordamp.ikonli.fontawesome;
+
+    opens io.github.darkaster.chess to javafx.fxml;
+    exports io.github.darkaster.chess;
+}
